8b66b11c2fb8b20c0b02d67fb6aa72dc5e9ba784
pisey <pisey@marinax.biz>
2019-01-03 11:22:48 +0700

    create and config app project

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by mp001 on 1/2/19.
+//  Copyright © 2019 mp001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
